f7c2720753ce0458a13d534786d426923a7fa513
Emanuele <egentili2000@gmail.com>
2025-01-04 12:03:26 +0100

    Created the file models_utils.py for containing some useful functions (so far contain one function that prints the model's variables). Created the class problem_instance to pack the all problem's data.

---
+++ SSCFL_AMOD/models_utils.py
@@ -0,0 +1,3 @@
+def print_solution(model):
+    for var in model.getVars():
+        print(f"{var.varName} = {var.x}")
---
+++ SSCFL_AMOD/problem_instance.py
@@ -0,0 +1,7 @@
+class ProblemInstance:
+    def __init__(self, facilities_opening_costs, transportation_costs, capacities, demands):
+        self.facilities_opening_costs = facilities_opening_costs
+        self.transportation_costs = transportation_costs
+        self.capacities = capacities
+        self.demands = demands
+        
