21464208ade5cffde44af86da57c98f18998c8f0
Emanuele <egentili2000@gmail.com>
2025-01-31 15:22:31 +0100

    Add heuristics.py and solve_lagrangian_relaxation.py

---
+++ SSCFL_AMOD/heuristics.py
@@ -0,0 +1,132 @@
+import models_utils as mu
+
+
+def initial_add_heuristic(problem_instance):
+    x = [0 for _ in range(problem_instance.num_facilities)]
+    y = [[0 for _ in range(problem_instance.num_customers)] for _ in range(problem_instance.num_facilities)]
+    # Step 0: start with K = [] and set Z_u = inf
+    k = []
+    z_u = float("inf")
+    # Step 1: foreach facility i not in k compute and estimate R_i of the saving that would result if i were added to K
+    w = [[0 for _ in range(problem_instance.num_customers)] for _ in range(problem_instance.num_facilities)]
+    omega = [0 for _ in range(problem_instance.num_customers)]
+    r = [0 for _ in range(problem_instance.num_facilities)]
+    j_list = [[j, 0] for j in range(problem_instance.num_customers)]
+    prohibit_k = []
+    while True:
+        for i in range(problem_instance.num_facilities):
+            if i not in k and len(k) != 0:
+                for j in range(problem_instance.num_customers):
+                    w[i][j] = max(compute_min(problem_instance, i, j, k), 0)
+                omega[i] = sum(w[i][j] for j in range(problem_instance.num_customers))
+                r[i] = omega[i] * min(problem_instance.capacities[i] /
+                                      sum(problem_instance.demands[j] for j in range(problem_instance.num_customers)
+                                          if w[i][j] > 0), 1) - problem_instance.facilities_opening_costs[i]
+        # Step 2: open facility i' that achieves the maximum R_i for i not in K and add i' to K
+        max_r = max(x for x in r if r.index(x) not in k)
+        i_p = r.index(max_r)
+        x[i_p] = 1
+        if i_p not in prohibit_k and i_p not in k:
+            k.append(i_p)
+        # Step 3: if sum(capacities[i] i in K) < sum(demands[j] j in range(num_customers) return to step 1. Otherwise
+        # continue
+        if sum(problem_instance.capacities[i] for i in k) >= sum(
+                problem_instance.demands[j] for j in range(problem_instance.num_customers)):
+            # Step 4: foreach customer j, computer the cost differential between its best and second-best assignment
+            # in K. Order j in order to decrease cos differential
+            for j in range(problem_instance.num_customers):
+                if len(k) != 0:
+                    first_best = compute_minus_on_column(problem_instance, j, float("inf"), k)
+                    second_best = compute_minus_on_column(problem_instance, j, first_best, k)
+                    j_list[j][1] = first_best - second_best
+            j_list_sorted = sorted(j_list, key=lambda j_elem: j_elem[1], reverse=True)
+            # Step 5: foreach j in order assign it to the open facility with minimum assignment cost among those with
+            # sufficient capacity. If for some j, no feasible assignment is possible, return to step 1. Otherwise
+            # continue
+            count_feasible = 0
+            for j in j_list_sorted:
+                feasible = []
+                for i in k:
+                    if ((sum(y[i][a] * problem_instance.demands[a] for a in range(problem_instance.num_customers)) +
+                         problem_instance.demands[j[0]] <= problem_instance.capacities[i] * x[i]) and
+                            sum(y[b][j[0]] for b in range(problem_instance.num_facilities))
+                            == 0):
+                        feasible.append(i)
+                        count_feasible += 1
+                if len(feasible) != 0:
+                    index = find_min_index(problem_instance, j[0], feasible)
+                    y[index][j[0]] = 1
+            if count_feasible != 0:
+                # Step 6: any facility with no customers assigned to it should be removed from K and not be
+                # re-entered in future iterations. If the total cost TC of the last feasible solution found is less
+                # than z_u, set z_u = TC and return to step (1). Otherwise stop
+                for i in range(problem_instance.num_facilities):
+                    if i in k and sum(y[i][j] for j in range(problem_instance.num_customers)) == 0:
+                        k.remove(i)
+                        prohibit_k.append(i)
+                obj_val = mu.obj_value(problem_instance, x, y)
+                if obj_val < z_u and mu.is_feasible(problem_instance, x, y):
+                    z_u = obj_val
+                    break
+                else:
+                    if mu.is_feasible(problem_instance, x, y):
+                        break
+    return x, y, z_u, k
+
+
+def final_adjustment_heuristic(problem_instance, best_x, best_y, k):
+    customers_list = [[j, 0] for j in range(problem_instance.num_customers)]
+    for j in range(problem_instance.num_customers):
+        if len(k) != 0:
+            first_best = compute_minus_on_column(problem_instance, j, float("inf"))
+            second_best = compute_minus_on_column(problem_instance, j, first_best)
+            customers_list[j][1] = first_best - second_best
+    customers_ordered = sorted(customers_list, key=lambda j_elem: j_elem[1], reverse=True)
+    for j in customers_ordered:
+        for i in range(problem_instance.num_facilities):
+            if best_y[i][j[0]] == 1:
+                best_y[i][j[0]] = 0
+                if not try_another_solution(problem_instance, best_x, best_y, j[0]):
+                    best_y[i][j[0]] = 1
+                    break
+
+
+def try_another_solution(problem_instance, x, y, j):
+    find_best_feasible = False
+    best_value = float("inf")
+    for i in range(problem_instance.num_facilities):
+        y[i][j] = 1
+        if mu.is_feasible(problem_instance, x, y):
+            feasible_value = mu.obj_value(problem_instance, x, y)
+            if feasible_value < best_value:
+                best_value = feasible_value
+                find_best_feasible = True
+                break
+    return find_best_feasible
+
+
+def compute_min(problem_instance, i, j, k):
+    res = float("inf")
+    c = problem_instance.transportation_costs
+    for s in k:
+        if c[s][j] - c[i][j] < res:
+            res = c[s][j] - c[i][j]
+    return res
+
+
+def compute_minus_on_column(problem_instance, j, div, k):
+    res = float("inf")
+    for i in k:
+        if problem_instance.transportation_costs[i][j] < res and problem_instance.transportation_costs[i][j] != div:
+            res = problem_instance.transportation_costs[i][j]
+    return res
+
+
+def find_min_index(problem_instance, j, feasible):
+    index = 0
+    res = float("inf")
+    for i in feasible:
+        if problem_instance.transportation_costs[i][j] < res:
+            res = problem_instance.transportation_costs[i][j]
+            index = i
+    return index
---
+++ SSCFL_AMOD/solve_lagrangian_relaxation.py
@@ -0,0 +1,106 @@
+import numpy as np
+import models as m
+import models_utils as mu
+import heuristics as h
+
+
+def solve_lagrangian_relaxation(problem_instance):
+    marked_lamb = []
+    lamb = np.zeros(problem_instance.num_facilities)
+    w = 0.25
+    epsilon = 0.0001
+    # Step 0: Solve (1) by the add heuristic and initialize z_u. Solve (2) with lambda_i = 0 and initialize z_l.
+    # If the solution of (2) is feasible for (1) stop. Otherwise, initialize the lambda_i for the violated constraints.
+    model = m.init_first_problem_relaxation(problem_instance, lamb)
+    model.setParam("OutputFlag", 0)
+    model.optimize()
+    z_l = model.objVal
+    best_x = get_x(model, problem_instance)
+    best_y = get_y(model, problem_instance)
+    best_x_feasible, best_y_feasible, z_u, k = h.initial_add_heuristic(problem_instance)
+    if not mu.is_feasible(problem_instance, best_x_feasible, best_y_feasible):
+        return
+    num_runs = 300
+    curr_relaxed_sol = model.objVal
+    it = 0
+    find_feasible = False
+    if mu.is_feasible(problem_instance, get_x(model, problem_instance), get_y(model, problem_instance)):
+        find_feasible = True
+        return
+    while True and num_runs >= 0:
+        print(f"Iteration {it}")
+        it += 1
+        prev_lamb = lamb
+        better_solution_found = False
+        for u in range(len(lamb)):
+            if u not in marked_lamb:
+                y = get_y(model, problem_instance)
+                x = get_x(model, problem_instance)
+                update_multipliers(problem_instance, lamb, marked_lamb, w, z_u, z_l, x, y, u)
+            if prev_lamb[u] > lamb[u]:
+                marked_lamb.append(u)
+            else:
+                if prev_lamb[u] < lamb[u] and u in marked_lamb:
+                    marked_lamb.remove(u)
+        # Step 1: Solve (2) and update z_l if necessary. If the solution is feasible for (1), update z_u if necessary
+        # and go to step 4. Otherwise, continue.
+        model = m.init_first_problem_relaxation(problem_instance, lamb)
+        model.setParam("OutputFlag", 0)
+        model.optimize()
+        curr_relaxed_sol = model.objVal
+        feasibility = mu.is_feasible(problem_instance, get_x(model, problem_instance), get_y(model, problem_instance))
+        if feasibility:
+            find_feasible = True
+        if curr_relaxed_sol > z_l >= 0:
+            z_l = curr_relaxed_sol
+            best_x = get_x(model, problem_instance)
+            best_y = get_y(model, problem_instance)
+        if feasibility and z_u > curr_relaxed_sol >= 0:
+            z_u = curr_relaxed_sol
+            best_x_feasible = get_x(model, problem_instance)
+            best_y_feasible = get_y(model, problem_instance)
+            better_solution_found = True
+        # Step 2: If (2) has been solved MAX times without providing another feasible solution to (1) go to step 5.
+        # Otherwise, continue.
+        if not better_solution_found:
+            num_runs -= 1
+            if num_runs == 0 and find_feasible:
+                h.final_adjustment_heuristic(problem_instance, best_x, best_y, k)
+        if better_solution_found or z_u / z_l <= 1 + epsilon:
+            if not find_feasible:
+                break
+            else:
+                h.final_adjustment_heuristic(problem_instance, best_x, best_y, k)
+                break
+        else:
+            marked_lamb = []
+    return z_u, best_x_feasible, best_y_feasible, z_l, best_x, best_y, lamb
+
+
+def get_x(model, problem_instance):
+    return [int(model.getVarByName(f"x[{u}]").X) for u in range(problem_instance.num_facilities)]
+
+
+def get_y(model, problem_instance):
+    return [[int(model.getVarByName(f"y[{u},{v}]").X) for v in range(problem_instance.num_customers)]
+            for u in range(problem_instance.num_facilities)]
+
+
+def compute_den(problem_instance, marked_lamb, y):
+    total = 0
+    facilities_total = 0
+    for i in range(problem_instance.num_facilities):
+        if i not in marked_lamb:
+            for j in range(problem_instance.num_customers):
+                total += problem_instance.demands[j] * y[i][j]
+            total -= problem_instance.capacities[i]
+            facilities_total += total ** 2
+    return facilities_total
+
+
+def update_multipliers(problem_instance, lamb, marked_lamb, w, z_u, z_l, x, y, u):
+    num = (w * (z_u - z_l) * (sum(problem_instance.demands[v] * y[u][v]
+                                  for v in range(problem_instance.num_customers))) -
+           problem_instance.capacities[u])
+    den = compute_den(problem_instance, marked_lamb, y)
+    lamb[u] = max(lamb[u] + num / den, 0)
